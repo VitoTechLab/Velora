25e2f63752234ad9a9d5d97d3e04fdf0550136cd
Vito28 <157898373+Vito28@users.noreply.github.com>
2025-07-19 23:24:24 +0700

    add site id in firebase.json

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:velora/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -11,9 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(child: Text("Hello World"))
-      )
+      home: Scaffold(body: Center(child: Text("Hello World"))),
     );
-}
+  }
 }
