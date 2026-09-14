391453b1774f5255429211e554aba6c84f366616
Vito28 <157898373+Vito28@users.noreply.github.com>
2026-09-14 10:24:45 +0700

    refactor(profile): migrate models and generated bindings

--- lib/features/profile/data/models/user_profile_model.g.dart
+++ lib/features/profile/data/models/user_profile_model.g.dart
@@ -6,17 +6,17 @@ part of 'user_profile_model.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-_$UserProfileModelImpl _$$UserProfileModelImplFromJson(
-        Map<String, dynamic> json) =>
-    _$UserProfileModelImpl(
+_UserProfileModel _$UserProfileModelFromJson(Map<String, dynamic> json) =>
+    _UserProfileModel(
       id: json['id'] as String,
       email: json['email'] as String? ?? '',
       username: json['username'] as String? ?? '',
       fullName: json['full_name'] as String?,
       avatarUrl: json['avatar_url'] as String?,
       bio: json['bio'] as String?,
-      dateOfBirth:
-          const NullableUtcDateTimeConverter().fromJson(json['date_of_birth']),
+      dateOfBirth: const NullableUtcDateTimeConverter().fromJson(
+        json['date_of_birth'],
+      ),
       gender: $enumDecodeNullable(_$UserGenderEnumMap, json['gender']),
       isOrganization: json['is_organization'] as bool? ?? false,
       organizationName: json['organization_name'] as String?,
@@ -37,8 +37,7 @@ _$UserProfileModelImpl _$$UserProfileModelImplFromJson(
       theyBlockedMe: json['they_blocked_me'] as bool? ?? false,
     );
 
-Map<String, dynamic> _$$UserProfileModelImplToJson(
-        _$UserProfileModelImpl instance) =>
+Map<String, dynamic> _$UserProfileModelToJson(_UserProfileModel instance) =>
     <String, dynamic>{
       'id': instance.id,
       'email': instance.email,
@@ -46,8 +45,9 @@ Map<String, dynamic> _$$UserProfileModelImplToJson(
       'full_name': instance.fullName,
       'avatar_url': instance.avatarUrl,
       'bio': instance.bio,
-      'date_of_birth':
-          const NullableUtcDateTimeConverter().toJson(instance.dateOfBirth),
+      'date_of_birth': const NullableUtcDateTimeConverter().toJson(
+        instance.dateOfBirth,
+      ),
       'gender': _$UserGenderEnumMap[instance.gender],
       'is_organization': instance.isOrganization,
       'organization_name': instance.organizationName,
