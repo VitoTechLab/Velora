06c6d3d6a76426dcf03da29312cf6ec8f133c628
Vito28 <157898373+Vito28@users.noreply.github.com>
2026-09-14 10:28:59 +0700

    build(android): update SDK NDK Gradle and build cache settings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ plugins {
 
 android {
     namespace = "com.example.velora"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 36
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
